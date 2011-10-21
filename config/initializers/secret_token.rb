f96ec1852bf36209d9e9fc5705dc29fb9c2b3cb8
Mariya Lysenkova <mariya@rampageconsulting.com>
2011-10-21 20:22:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alchemist::Application.config.secret_token = '59a03cc73f3b0d26e71a1254dd55cef87c87b9a244826ee1fcf785818317c1c3e9e8ea9561dede981f658c830848bbc7a999315ef998fdbc7bdb68d920d93323'
