78d3a2d673dedd96ff67dcc1ada9aa5188df2074
Mariya Lysenkova <mariya@rampageconsulting.com>
2012-04-11 23:15:34 +0200

    Got a draft of heatmaps working

--- app/controllers/municipalities_controller.rb
+++ app/controllers/municipalities_controller.rb
@@ -8,10 +8,15 @@ class MunicipalitiesController < InheritedResources::Base
   def top_connections
     @title = "Top #{@limit} municipalities, by number of potential connections"
     @json = Municipality.find(:all, :order => "num_connections desc", :limit => @limit).to_gmaps4rails
+        render :layout => "heatmap"
   end
 
   def top_factor
     @title = "Top #{@limit} municipalities, by mean size of potential connections"
     @json = Municipality.find(:all, :order => "mean_connections_factor desc", :limit => @limit).to_gmaps4rails
+  end
+
+  def heatmap
+    muns = Municipality.find(:all, :order => "mean_connections_factor desc", :limit => @limit)
   end
 end
